d6c14b760ee690d59ddb3d4272d3301fe48f4542
BrCOS <cosbrnta@gmail.com>
2024-09-12 04:37:00 -0300

    docs: add day 01 README.md

---
+++ Day 01/day01.js
@@ -0,0 +1,19 @@
+// console.log()
+
+console.log('Hello World'); //same as python print statement
+
+// constants
+
+const name = 'Bruno'; //onde you assign a vlaue here it can't be changed
+                        //(similar as tuples in python, but as if they were variables)
+
+// variables
+
+let lastName = 'Costa'; //that's how we declare variables
+
+// let vs var
+
+var fullName = 'Bruno Costa'; //they are pretty much the same, but the 'var' declares it on a global level
+                                //even if declared inside of a scope (function) as a temp variable
+                                //so, always use let
+
