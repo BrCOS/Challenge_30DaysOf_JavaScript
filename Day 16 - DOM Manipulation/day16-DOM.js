1a03aaa5319dd0b51e8ab4c04c20f68ae8f79c71
BrCOS <cosbrnta@gmail.com>
2024-10-08 01:23:10 -0300

    feat: add day 16 js file

---
+++ Day 16 - DOM Manipulation/day16-DOM.js
@@ -0,0 +1,8 @@
+// the dom
+//tags and structure that they provide tells the browser how to render the page
+
+//loaded into the Document Object Model (DOM), this means that the browser takes
+//the HTML and converts it into a model of the page that is able to render
+
+//the HTML that we write and gets displayed never changes no matter what we do with the JS
+//that is because the js doesn't change the HTML (that we see), it changes the DOM (thing your browser creats)
